d1d69383fba04e94e9677842429d2ac76680644d
farhad <rb0214192@gmail.com>
2022-11-25 16:24:12 +0330

    app component updated with layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import "./App.css";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
-  return <div>
-    
-  </div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <div className="app">
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout>app</Layout>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </div>
+  );
 }
 
 export default App;
