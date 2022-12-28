5f5187c61c5a70cff468e4754e807e87b095219b
farhad <rb0214192@gmail.com>
2022-12-28 19:25:05 +0330

    drawer added

--- src/components/topBar/index.jsx
+++ src/components/topBar/index.jsx
@@ -10,31 +10,22 @@ import {
   Typography,
   useTheme,
   Badge,
-  Menu,
-  MenuItem,
-  Divider,
-  ListItemIcon,
+  Drawer,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { tokens } from "../../theme";
 import avatar from "../../assets/avatar.jpg";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { useState } from "react";
-import { Logout, Settings } from "@mui/icons-material";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import MailIcon from "@mui/icons-material/Mail";
-import HelpIcon from "@mui/icons-material/Help";
+import SideBar from "../sideBar";
+import AvatarMenu from "./avatarMenu";
+
 const TopBar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const clickHandler = (e) => {
-    setAnchorEl(e.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const [openMenu, setOpenMenu] = useState(null);
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const open = Boolean(openMenu);
   const CssTextField = styled(TextField)({
     "& label.Mui-focused": {
       color: colors.primary[100],
@@ -54,6 +45,7 @@ const TopBar = () => {
       },
     },
   });
+
   return (
     <AppBar
       color="transparent"
@@ -62,15 +54,32 @@ const TopBar = () => {
     >
       <Toolbar sx={{ justifyContent: "space-between" }}>
         <Box display={"flex"} flexDirection="row" alignItems="center">
-          <IconButton size="large" edge="start" aria-label="open drawer">
+          <IconButton
+            onClick={() => setOpenDrawer(true)}
+            size="large"
+            edge="start"
+            sx={{ display: { sm: "inline-block", md: "none" } }}
+          >
             <MenuIcon />
           </IconButton>
           <Typography
-            variant="body"
+            variant="h6"
             sx={{ display: { xs: "none", sm: "block" } }}
           >
             Dashboard
           </Typography>
+          <Drawer
+            anchor={"left"}
+            open={openDrawer}
+            onClose={() => setOpenDrawer(false)}
+            PaperProps={{
+              sx: {
+                backgroundColor: colors.primary[600],
+              },
+            }}
+          >
+            <SideBar setOpenDrawer={setOpenDrawer} />
+          </Drawer>
         </Box>
         <Box display={"flex"} flexDirection="row" alignItems="center">
           <CssTextField
@@ -89,7 +98,7 @@ const TopBar = () => {
               <NotificationsIcon color="action" />
             </Badge>
           </IconButton>
-          <IconButton onClick={clickHandler}>
+          <IconButton onClick={(e) => setOpenMenu(e.currentTarget)}>
             <Badge
               color={"success"}
               variant="dot"
@@ -102,78 +111,7 @@ const TopBar = () => {
             </Badge>
           </IconButton>
         </Box>
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
-          open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          PaperProps={{
-            elevation: 0,
-            sx: {
-              overflow: "visible",
-              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-              mt: 1.5,
-              color: colors.primary[900],
-              bgcolor: colors.primary[300],
-              "& .MuiAvatar-root": {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1,
-                color: colors.primary[900],
-                bgcolor: colors.primary[200],
-              },
-              "&:before": {
-                content: '""',
-                display: "block",
-                position: "absolute",
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                color: colors.primary[900],
-                bgcolor: colors.primary[300],
-                transform: "translateY(-50%) rotate(45deg)",
-                zIndex: 0,
-              },
-            },
-          }}
-          transformOrigin={{ horizontal: "right", vertical: "top" }}
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        >
-          <MenuItem>
-            <ListItemIcon>
-              <AccountCircleIcon />
-            </ListItemIcon>
-            Profile
-          </MenuItem>
-          <MenuItem>
-            <ListItemIcon>
-              <Settings />
-            </ListItemIcon>
-            Settings
-          </MenuItem>
-          <Divider />
-          <MenuItem>
-            <ListItemIcon>
-              <MailIcon />
-            </ListItemIcon>
-            Inbox
-          </MenuItem>
-          <MenuItem>
-            <ListItemIcon>
-              <HelpIcon />
-            </ListItemIcon>
-            Need help?
-          </MenuItem>
-          <MenuItem>
-            <ListItemIcon>
-              <Logout />
-            </ListItemIcon>
-            Logout
-          </MenuItem>
-        </Menu>
+        <AvatarMenu open={open} openMenu={openMenu} setOpenMenu={setOpenMenu} />
       </Toolbar>
     </AppBar>
   );
