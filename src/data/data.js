60b73163ca3002f2b060583eae18000628c084e3
farhad <rb0214192@gmail.com>
2022-12-30 18:11:31 +0330

    update

--- src/data/data.js
+++ src/data/data.js
@@ -1,54 +1,70 @@
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import AppsIcon from "@mui/icons-material/Apps";
-import GridViewIcon from "@mui/icons-material/GridView";
-import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import DescriptionIcon from "@mui/icons-material/Description";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import ArticleIcon from "@mui/icons-material/Article";
+import {
+  Dashboard,
+  GridView,
+  Logout,
+  Help,
+  Article,
+  BarChart,
+  Description,
+  BusinessCenter,
+  AccountCircle,
+  Settings,
+  Mail,
+  Apps,
+} from "@mui/icons-material";
+
 export const menuItems = [
-  { title: "Dashboard", slug: "/", id: "0", icon: DashboardIcon },
+  { title: "dashboard", slug: "/", id: "0", icon: Dashboard },
   {
     title: "Apps",
     id: "1",
-    icon: AppsIcon,
+    icon: Apps,
     subItems: [
-      { title: "Inbox", id: "11", slug: "/inbox" },
-      { title: "Contacts", id: "12", slug: "/contacts" },
-      { title: "Calendar", id: "13", slug: "/calendar" },
-      { title: "Note", id: "14", slug: "/note" },
+      { title: "Inbox", id: "11", slug: "/apps/inbox" },
+      { title: "Contacts", id: "12", slug: "/apps/contacts" },
+      { title: "Calendar", id: "13", slug: "/apps/calendar" },
+      { title: "Note", id: "14", slug: "/apps/note" },
     ],
   },
   {
     title: "Layouts",
     id: "2",
-    icon: GridViewIcon,
+    icon: GridView,
     subItems: [
-      { title: "Header", id: "21", slug: "/header" },
-      { title: "Aside", id: "22", slug: "/aside" },
-      { title: "Footer", id: "23", slug: "/footer" },
+      { title: "Header", id: "21", slug: "/layouts/header" },
+      { title: "Aside", id: "22", slug: "/layouts/aside" },
+      { title: "Footer", id: "23", slug: "/layouts/footer" },
     ],
   },
-  { title: "UI Kits", slug: "/kits", id: "3", icon: BusinessCenterIcon },
+  { title: "UI Kits", slug: "/kits", id: "3", icon: BusinessCenter },
   {
     title: "Pages",
     id: "4",
-    icon: DescriptionIcon,
+    icon: Description,
     subItems: [
-      { title: "Profile", id: "41", slug: "/profile" },
-      { title: "Settings", id: "42", slug: "/settings" },
-      { title: "Search", id: "43", slug: "/search" },
-      { title: "FAQ", id: "44", slug: "/faq" },
+      { title: "Profile", id: "41", slug: "/pages/profile" },
+      { title: "Settings", id: "42", slug: "/pages/settings" },
+      { title: "Search", id: "43", slug: "/pages/search" },
+      { title: "FAQ", id: "44", slug: "/pages/faq" },
     ],
   },
   {
     title: "Charts",
     id: "5",
-    icon: BarChartIcon,
+    icon: BarChart,
     subItems: [
-      { title: "Line", id: "51", slug: "/line" },
-      { title: "Bar", id: "52", slug: "/bar" },
-      { title: "Pie", id: "53", slug: "/pie" },
+      { title: "Line", id: "51", slug: "/charts/line" },
+      { title: "Bar", id: "52", slug: "/charts/bar" },
+      { title: "Pie", id: "53", slug: "/charts/pie" },
     ],
   },
-  { title: "Document", slug: "/document", id: "6", icon: ArticleIcon },
+  { title: "Document", slug: "/document", id: "6", icon: Article },
+];
+
+export const avatarMenuItems = [
+  { id: 1, title: "Profile", to:"/pages/profile", icon: AccountCircle },
+  { id: 2, title: "Settings",to:"/pages/settings", icon: Settings },
+  { id: 3, title: "Inbox",to:"/apps/inbox", icon: Mail },
+  { id: 4, title: " Need help?",to:"/pages/faq", icon: Help },
+  { id: 5, title: " Logout",to:"/logout", icon: Logout },
 ];
