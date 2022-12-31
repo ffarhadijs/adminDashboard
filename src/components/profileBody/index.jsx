bca78f530f52ab2d8b9df7176ef6ee80f1231ea0
farhad <rb0214192@gmail.com>
2023-01-01 00:10:10 +0330

    add tabs and panels

--- src/components/profileBody/index.jsx
+++ src/components/profileBody/index.jsx
@@ -12,6 +12,7 @@ import { tokens } from "../../theme";
 import TabPanel from "../tabPanel";
 import Friends from "./friends";
 import ProfileData from "./profileData";
+import Grid from "@mui/material/Unstable_Grid2";
 
 export default function ProfileBody() {
   const [value, setValue] = useState(0);
@@ -21,68 +22,75 @@ export default function ProfileBody() {
     setValue(newValue);
   };
   return (
-    <Box sx={{ width: "100%" }}>
-      <Stack
-        flexDirection={{ xs: "column-reverse", sm: "row" }}
-        justifyContent="space-between"
-        alignItems={"center"}
-        sx={{ backgroundColor: colors.primary[600], padding: "20px" }}
-      >
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          textColor="secondary"
-          indicatorColor="secondary"
-        >
-          <Tab label="Friends" />
-          <Tab label="Profile" />
-        </Tabs>
+    <Grid container xs={12} sx={{ width: "100%" }}>
+      <Grid xs={12}>
         <Stack
-          flexDirection={"row"}
-          alignItems="center"
-          flexWrap={"nowrap"}
-          gap={2}
+          flexDirection={{ xs: "column-reverse", sm: "row" }}
+          justifyContent="space-between"
+          alignItems={"center"}
+          sx={{ backgroundColor: colors.primary[600], padding: "20px" }}
         >
-          <Box textAlign={"center"}>
-            <Typography variant="h5">2K</Typography>
-            <Typography
-              variant="subtitle2"
-              color={colors.grey[400]}
-              fontSize={"small"}
-            >
-              Followers
-            </Typography>
-          </Box>
-          <Divider orientation="vertical" flexItem />
-          <Box textAlign={"center"}>
-            <Typography variant="h5">250</Typography>
-            <Typography
-              variant="subtitle2"
-              color={colors.grey[400]}
-              fontSize={"small"}
-            >
-              Following
-            </Typography>
-          </Box>
-          <Divider orientation="vertical" flexItem />
-          <Box textAlign={"center"}>
-            <Typography variant="h5">89</Typography>
-            <Typography
-              variant="subtitle2"
-              color={colors.grey[400]}
-              fontSize={"small"}
-            >
-              Activities
-            </Typography>
-          </Box>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
+            <Tab label="Friends" />
+            <Tab label="Profile" />
+          </Tabs>
+          <Stack
+            flexDirection={"row"}
+            alignItems="center"
+            flexWrap={"nowrap"}
+            gap={2}
+          >
+            <Box textAlign={"center"}>
+              <Typography variant="h5">2K</Typography>
+              <Typography
+                variant="subtitle2"
+                color={colors.grey[400]}
+                fontSize={"small"}
+              >
+                Followers
+              </Typography>
+            </Box>
+            <Divider orientation="vertical" flexItem />
+            <Box textAlign={"center"}>
+              <Typography variant="h5">250</Typography>
+              <Typography
+                variant="subtitle2"
+                color={colors.grey[400]}
+                fontSize={"small"}
+              >
+                Following
+              </Typography>
+            </Box>
+            <Divider orientation="vertical" flexItem />
+            <Box textAlign={"center"}>
+              <Typography variant="h5">89</Typography>
+              <Typography
+                variant="subtitle2"
+                color={colors.grey[400]}
+                fontSize={"small"}
+              >
+                Activities
+              </Typography>
+            </Box>
+          </Stack>
         </Stack>
-      </Stack>
-      <TabPanel value={value} index={0}>
-        <Friends />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <ProfileData />
-      </TabPanel>
-    </Box>
+      </Grid>
+      <Grid xs={12} sm={8} >
+        <TabPanel value={value} index={0}>
+          <Friends />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <ProfileData />
+        </TabPanel>
+      </Grid>
+      <Grid xs={12} sm={4} p={"24px"}>
+        <Box sx={{ backgroundColor: "white", height: "200px" }}></Box>
+      </Grid>
+    </Grid>
   );
 }
