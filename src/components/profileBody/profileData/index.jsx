557670866eacf3b8c38a5ecff3d30b3ecb8c43c1
farhad <rb0214192@gmail.com>
2023-01-01 00:10:43 +0330

    create profile Data component

--- src/components/profileBody/profileData/index.jsx
+++ src/components/profileBody/profileData/index.jsx
@@ -1,9 +1,69 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import { tokens } from "../../../theme";
 
-export default function ProfileData(){
-return (
-    <Box>
-        profile Data
-    </Box>
-)
+export default function ProfileData() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Grid container rowGap={4}>
+      <Grid xs={6}>
+        <Typography
+          variant="subtitle2"
+          fontSize="small"
+          color={colors.grey[400]}
+        >
+          Full Name
+        </Typography>
+        <Typography variant="subtitle2">Farhad Farhadi</Typography>
+      </Grid>
+      <Grid xs={6}>
+        <Typography
+          variant="subtitle2"
+          fontSize="small"
+          color={colors.grey[400]}
+        >
+          Cell Phone
+        </Typography>
+        <Typography variant="subtitle2">+1 427-332-5682</Typography>
+      </Grid>
+      <Grid xs={6}>
+        <Typography
+          variant="subtitle2"
+          fontSize="small"
+          color={colors.grey[400]}
+        >
+          Job
+        </Typography>
+        <Typography variant="subtitle2">Front-end developer</Typography>
+      </Grid>
+      <Grid xs={6}>
+        <Typography
+          variant="subtitle2"
+          fontSize="small"
+          color={colors.grey[400]}
+        >
+          Location
+        </Typography>
+        <Typography variant="subtitle2">Iran,Urmia</Typography>
+      </Grid>
+      <Grid xs={12}>
+        <Typography
+          variant="subtitle2"
+          fontSize="small"
+          color={colors.grey[400]}
+        >
+          Bio
+        </Typography>
+        <Typography variant="subtitle2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facilis
+          rem porro autem, pariatur veritatis sed eaque quia! Aperiam aspernatur
+          nobis eius non dignissimos obcaecati nostrum nihil est cupiditate
+          magni hic inventore iste vel aut commodi quisquam tempore, odio
+          aliquam. Cupiditate dolorum laudantium incidunt dicta illo porro
+          cumque, sit animi?
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
