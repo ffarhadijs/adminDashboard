f9cfbab8515977429fc6edab9b7e1503ac899295
farhad <rb0214192@gmail.com>
2023-01-05 02:27:11 +0330

    make dynamic profile data

--- src/components/profileBody/profileData/index.jsx
+++ src/components/profileBody/profileData/index.jsx
@@ -5,6 +5,8 @@ import { tokens } from "../../../theme";
 export default function ProfileData() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const localValues = JSON.parse(localStorage.getItem("profileData"));
+
   return (
     <Grid container rowGap={4}>
       <Grid xs={6}>
@@ -15,7 +17,9 @@ export default function ProfileData() {
         >
           Full Name
         </Typography>
-        <Typography variant="subtitle2">Farhad Farhadi</Typography>
+        <Typography variant="subtitle2">
+          {localValues?.firstName} {localValues?.lastName}
+        </Typography>
       </Grid>
       <Grid xs={6}>
         <Typography
@@ -25,7 +29,17 @@ export default function ProfileData() {
         >
           Cell Phone
         </Typography>
-        <Typography variant="subtitle2">+1 427-332-5682</Typography>
+        <Typography variant="subtitle2">{localValues?.cellPhone}</Typography>
+      </Grid>
+      <Grid xs={6}>
+        <Typography
+          variant="subtitle2"
+          fontSize="small"
+          color={colors.grey[400]}
+        >
+          Email
+        </Typography>
+        <Typography variant="subtitle2">{localValues?.email}</Typography>
       </Grid>
       <Grid xs={6}>
         <Typography
@@ -35,7 +49,7 @@ export default function ProfileData() {
         >
           Job
         </Typography>
-        <Typography variant="subtitle2">Front-end developer</Typography>
+        <Typography variant="subtitle2">{localValues?.job}</Typography>
       </Grid>
       <Grid xs={6}>
         <Typography
@@ -45,7 +59,8 @@ export default function ProfileData() {
         >
           Location
         </Typography>
-        <Typography variant="subtitle2">Iran,Urmia</Typography>
+        <Typography variant="subtitle2">{localValues?.country}</Typography>{" "}
+        <Typography variant="subtitle2">{localValues?.state}</Typography>
       </Grid>
       <Grid xs={12}>
         <Typography
@@ -55,14 +70,7 @@ export default function ProfileData() {
         >
           Bio
         </Typography>
-        <Typography variant="subtitle2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facilis
-          rem porro autem, pariatur veritatis sed eaque quia! Aperiam aspernatur
-          nobis eius non dignissimos obcaecati nostrum nihil est cupiditate
-          magni hic inventore iste vel aut commodi quisquam tempore, odio
-          aliquam. Cupiditate dolorum laudantium incidunt dicta illo porro
-          cumque, sit animi?
-        </Typography>
+        <Typography variant="subtitle2">{localValues?.bio}</Typography>
       </Grid>
     </Grid>
   );
