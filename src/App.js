c98429165403a40e6252dc26594cc9bfa878b41d
farhad <rb0214192@gmail.com>
2023-01-05 02:36:41 +0330

    add settings page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { ColorModeContext, useMode } from "./theme";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
+import Settings from "./pages/Settings";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -18,6 +19,7 @@ function App() {
           <Layout>
             <Routes>
               <Route path="/pages/profile" element={<Profile />} />
+              <Route path="/pages/settings" element={<Settings />} />
               <Route path="/" element={<Home />} />
             </Routes>
           </Layout>
