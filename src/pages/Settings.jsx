c1d23eac225190e82e6ddc01ffec0e82fab98fa6
farhad <rb0214192@gmail.com>
2023-01-05 02:35:59 +0330

    settings page with profile form component

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import UpdateForm from "../components/updateForm";
+
+export default function Settings() {
+  return (
+    <Box width={"100%"}>
+      <UpdateForm />
+    </Box>
+  );
+}
