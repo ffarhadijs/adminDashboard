34b9b3b16ae2305c20b1ffe5c07f53d8d0104496
farhad <rb0214192@gmail.com>
2023-01-18 15:00:44 +0330

    add charts data

--- src/data/data.js
+++ src/data/data.js
@@ -12,15 +12,15 @@ import {
   Mail,
   Apps,
 } from "@mui/icons-material";
-import user1 from "../assets/avatar.jpg"
-import user2 from "../assets/user (1).jpg"
-import user3 from "../assets/user (2).jpg"
-import user4 from "../assets/user (3).jpg"
-import user5 from "../assets/user (4).jpg"
-import user6 from "../assets/user (5).jpg"
-import user7 from "../assets/user (6).jpg"
-import user8 from "../assets/user (7).jpg"
-import user9 from "../assets/user (8).jpg"
+import user1 from "../assets/avatar.jpg";
+import user2 from "../assets/user (1).jpg";
+import user3 from "../assets/user (2).jpg";
+import user4 from "../assets/user (3).jpg";
+import user5 from "../assets/user (4).jpg";
+import user6 from "../assets/user (5).jpg";
+import user7 from "../assets/user (6).jpg";
+import user8 from "../assets/user (7).jpg";
+import user9 from "../assets/user (8).jpg";
 
 export const menuItems = [
   { title: "dashboard", slug: "/", id: "0", icon: Dashboard },
@@ -78,14 +78,80 @@ export const avatarMenuItems = [
   { id: 5, title: " Logout", to: "/logout", icon: Logout },
 ];
 
-export const users=[
-  {id:1, name:"Crystal Guerrero", job:"Designer, Blogger", avatar:user1, color:"success" },
-  {id:2, name:"James Garcia", job:"Writter, Mag Editor", avatar:user2, color:"error" },
-  {id:3, name:"Jean Schneider", job:"Designer, Blogger", avatar:user3, color:"success" },
-  {id:4, name:"Joe Holmes", job:"Front-end Developer", avatar:user4, color:"error" },
-  {id:6, name:"Richard Carr", job:"Product Designer", avatar:user5, color:"error" },
-  {id:7, name:"Sara King", job:"Teacher", avatar:user6, color:"success" },
-  {id:8, name:"Melissa Garza", job:"Producer", avatar:user7, color:"success" },
-  {id:9, name:"Jeremy Scott", job:"Writer", avatar:user8, color:"error" },
-  {id:5, name:"OlsJesse Russell", job:"Back-end Developer", avatar:user9, color:"success" },
-]
+export const users = [
+  {
+    id: 1,
+    name: "Crystal Guerrero",
+    job: "Designer, Blogger",
+    avatar: user1,
+    color: "success",
+  },
+  {
+    id: 2,
+    name: "James Garcia",
+    job: "Writter, Mag Editor",
+    avatar: user2,
+    color: "error",
+  },
+  {
+    id: 3,
+    name: "Jean Schneider",
+    job: "Designer, Blogger",
+    avatar: user3,
+    color: "success",
+  },
+  {
+    id: 4,
+    name: "Joe Holmes",
+    job: "Front-end Developer",
+    avatar: user4,
+    color: "error",
+  },
+  {
+    id: 6,
+    name: "Richard Carr",
+    job: "Product Designer",
+    avatar: user5,
+    color: "error",
+  },
+  { id: 7, name: "Sara King", job: "Teacher", avatar: user6, color: "success" },
+  {
+    id: 8,
+    name: "Melissa Garza",
+    job: "Producer",
+    avatar: user7,
+    color: "success",
+  },
+  { id: 9, name: "Jeremy Scott", job: "Writer", avatar: user8, color: "error" },
+  {
+    id: 5,
+    name: "OlsJesse Russell",
+    job: "Back-end Developer",
+    avatar: user9,
+    color: "success",
+  },
+];
+
+export const emailsData = [
+  { id: 3, year: 2019, emails: 180 },
+  { id: 4, year: 2020, emails: 97 },
+  { id: 5, year: 2021, emails: 121 },
+  { id: 6, year: 2022, emails: 165 },
+  { id: 7, year: 2023, emails: 125 },
+];
+
+export const projectsData = [
+  { id: 3, year: 2019, projects: 37 },
+  { id: 4, year: 2020, projects: 52 },
+  { id: 5, year: 2021, projects: 15 },
+  { id: 6, year: 2022, projects: 45 },
+  { id: 7, year: 2023, projects: 40 },
+];
+
+export const usersData = [
+  { id: 3, year: 2019, users: 350 },
+  { id: 4, year: 2020, users: 559},
+  { id: 5, year: 2021, users: 421 },
+  { id: 6, year: 2022, users: 365 },
+  { id: 7, year: 2023, users: 600 },
+];
