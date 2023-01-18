533bf786ec697243bef782b00ee9488f11fcdcfc
farhad <rb0214192@gmail.com>
2023-01-18 14:45:50 +0330

    add tiles to main page

---
+++ src/components/dashboardInfoTile/index.jsx
@@ -0,0 +1,53 @@
+import { Email } from "@mui/icons-material"
+import { Icon, Paper, Typography, useTheme } from "@mui/material"
+import { Box } from "@mui/system"
+import { tokens } from "../../theme"
+import Avatar from "../updateForm/avatar"
+
+const DashboardInfoTile=({avatar,number,title,subTitle, backgroundColor})=>{
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return(
+        <>
+          <Paper
+            elevation={4}
+            sx={{
+              backgroundColor: colors.primary[600],
+              height: "auto",
+              padding: "10px",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "start",
+              columnGap: "20px",
+            }}
+          >
+            <Box
+              sx={{ backgroundColor: {backgroundColor}, width: 48, height: 48, borderRadius:"100%", display:"flex", justifyContent:"center",alignItems:"center", color:"black" }}
+              >
+              {avatar}
+            </Box>
+            <Box>
+              <Typography variant="h5" component={"span"}>
+                {" "}
+                {number}
+              </Typography>
+              <Typography variant="subtitle2" component={"span"}>
+                {" "}
+                {title}
+              </Typography>
+              <Typography
+                variant="caption"
+                component={"div"}
+                color={colors.grey[200]}
+              >
+                {subTitle}
+              </Typography>
+            </Box>
+          </Paper>
+        </>
+    )
+}
+export default DashboardInfoTile
