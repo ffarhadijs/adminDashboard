834d59114c3631bfb8e6f61b592416dda3620d64
farhad <rb0214192@gmail.com>
2023-01-19 12:57:47 +0330

    add some pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import "./App.css";
 import { ColorModeContext, useMode } from "./theme";
@@ -7,6 +7,16 @@ import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Settings from "./pages/Settings";
+import Inbox from "./pages/Inbox";
+import InboxEmails from "./pages/InboxEmails";
+import SentEmails from "./pages/SentEmails";
+import TrashEmails from "./pages/TrashEmails";
+import InboxLayout from "./components/inboxLayout";
+import InboxReact from "./components/inboxReact";
+import InboxAngular from "./components/inboxAngular";
+import ReactEmail from "./pages/ReactEmail";
+import AngularEmail from "./pages/AngularEmail";
+import VueEmail from "./pages/VueEmail";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -21,6 +31,13 @@ function App() {
               <Route path="/pages/profile" element={<Profile />} />
               <Route path="/pages/settings" element={<Settings />} />
               <Route path="/" element={<Home />} />
+              <Route path="/apps/inbox" element={<Inbox />} />
+              <Route path="/apps/inbox/inbox" element={<InboxEmails />} />
+              <Route path="/apps/inbox/sent" element={<SentEmails />} />
+              <Route path="/apps/inbox/trash" element={<TrashEmails />} />
+              <Route path="/apps/inbox/react" element={<ReactEmail />} />
+              <Route path="/apps/inbox/angular" element={<AngularEmail />} />
+              <Route path="/apps/inbox/vue" element={<VueEmail />} />
             </Routes>
           </Layout>
         </ThemeProvider>
