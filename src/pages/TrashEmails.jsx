f974d1d80097ea7e6d7fab1f546a7d605d097bf5
farhad <rb0214192@gmail.com>
2023-01-19 12:56:58 +0330

    create trash emails page

---
+++ src/pages/TrashEmails.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import InboxLayout from "../components/inboxLayout";
+import Trash from "../components/trash";
+
+export default function TrashEmails(){
+
+    return(
+        <InboxLayout>
+        <Trash/>
+    </InboxLayout>
+    )
+}
