e897a29d451e46820ed60c491793c888f0bcf8b4
farhad <rb0214192@gmail.com>
2023-01-19 12:57:22 +0330

    create vue label emails page

---
+++ src/pages/VueEmail.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import InboxLayout from "../components/inboxLayout";
+import InboxVue from "../components/inboxVue";
+
+export default function VueEmail() {
+  return (
+    <InboxLayout>
+    <InboxVue/>
+</InboxLayout>
+  );
+}
