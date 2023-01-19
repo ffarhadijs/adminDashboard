c96ab4d241620acbfe91aa1291876cad7fb89431
farhad <rb0214192@gmail.com>
2023-01-19 12:56:21 +0330

    create react label emails page

---
+++ src/pages/ReactEmail.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import InboxLayout from "../components/inboxLayout";
+import InboxReact from "../components/inboxReact";
+
+export default function ReactEmail(){
+
+return(
+    <InboxLayout>
+        <InboxReact/>
+    </InboxLayout>
+)
+}
