5be78616617f4b87eeff3599cf846297073528fb
farhad <rb0214192@gmail.com>
2023-01-19 12:48:01 +0330

    create sidebar for emails components

---
+++ src/components/inboxSideBar/index.jsx
@@ -0,0 +1,64 @@
+import { Button, Stack, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { inboxLabels, inboxSideBar } from "../../data/data";
+import { Link, useLocation } from "react-router-dom";
+const InboxSideBar = () => {
+  const location = useLocation();
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Stack
+      flexDirection={"column"}
+      alignItems="start"
+      justifyContent={"start"}
+      sx={{
+        width: "150px",
+        height: "auto",
+        minHeight: "100vh",
+        backgroundColor: colors.primary[700],
+        padding: "20px",
+      }}
+    >
+      <Button>compose</Button>
+      {inboxSideBar.map((item) => (
+        <Link
+          key={item.id}
+          to={`/${item.slug}`}
+          style={{
+            fontSize:"14px",
+            display: "block",
+            width: "100%",
+            backgroundColor: location.pathname.includes(item.slug) && "#4cceac",
+            padding: "5px",
+            marginTop: "5px",
+            borderRadius: "7px",
+          }}
+        >
+          {item.title}
+        </Link>
+      ))}
+      <Typography variant="subtitle1" sx={{ padding: "30px 0 5px 0" }}>
+        Labels
+      </Typography>
+      {inboxLabels.map((item) => (
+        <Link
+          key={item.id}
+          to={`/${item.slug}`}
+          style={{
+            fontSize:"14px",
+            display: "block",
+            width: "100%",
+            backgroundColor: location.pathname.includes(item.slug) && "#4cceac",
+            padding: "5px",
+            marginTop: "5px",
+            borderRadius: "7px",
+          }}
+        >
+          {item.title}
+        </Link>
+      ))}
+    </Stack>
+  );
+};
+
+export default InboxSideBar;
