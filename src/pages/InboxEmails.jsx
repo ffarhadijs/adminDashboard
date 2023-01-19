d8d3c678e91deb050631c98a5ddf1c751c21ae6a
farhad <rb0214192@gmail.com>
2023-01-19 12:54:57 +0330

    create inbox emails page

---
+++ src/pages/InboxEmails.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Inbox from "../components/inbox";
+import InboxLayout from "../components/inboxLayout";
+
+export default function InboxEmails(){
+
+return(
+<InboxLayout>
+    <Inbox/>
+</InboxLayout>
+)
+}
