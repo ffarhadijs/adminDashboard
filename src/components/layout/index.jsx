a52837f4209d0c523dd659fe7b92e60f883ab5da
farhad <rb0214192@gmail.com>
2023-01-19 12:50:06 +0330

    update style

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,14 +1,12 @@
 import { Box, useTheme } from "@mui/material";
 import React from "react";
-import { useContext } from "react";
-import { ColorModeContext, tokens } from "../../theme";
+import { tokens } from "../../theme";
 import SideBar from "../sideBar";
 import TopBar from "../topBar";
 
 const Layout = ({ children }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const colorMode = useContext(ColorModeContext);
 
   return (
     <Box
@@ -24,7 +22,8 @@ const Layout = ({ children }) => {
           backgroundColor: colors.primary[600],
           height: "auto",
           minHeight: "100vh",
-          display:{xs:"none",sm:"none",md:"block"}
+          display:{xs:"none",sm:"none",md:"block"},
+          zIndex:"1001"
         }}
       >
         <SideBar />
@@ -40,8 +39,10 @@ const Layout = ({ children }) => {
         <Box
           sx={{
             backgroundColor: colors.primary[700],
+            boxShadow:"rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px",
             width: "100%",
             height: "60px",
+            zIndex:"1000"
           }}
         >
           <TopBar />
