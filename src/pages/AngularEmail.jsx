ae943ae0840999d53586668c6a8c97dfc7e0138d
farhad <rb0214192@gmail.com>
2023-01-19 12:53:35 +0330

    create angular label page

---
+++ src/pages/AngularEmail.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import InboxAngular from "../components/inboxAngular";
+import InboxLayout from "../components/inboxLayout";
+
+export default function AngularEmail() {
+  return (
+    <InboxLayout>
+      <InboxAngular />
+    </InboxLayout>
+  );
+}
