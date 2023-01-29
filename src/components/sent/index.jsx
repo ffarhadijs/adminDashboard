244722063d8dc0f9aa69541cbb7d1f325d96c24d
farhad <rb0214192@gmail.com>
2023-01-29 10:38:43 +0330

    refactor:add usequery and emailslist component

--- src/components/sent/index.jsx
+++ src/components/sent/index.jsx
@@ -1,10 +1,32 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import EmailsList from "../emailsList";
+import { useQuery } from "react-query";
+import { useFetchEmails } from "../../hooks/useFetchEmails";
+import { Loading } from "../loading";
 
-export default function Sent (){
+export default function Sent() {
+  const { data, status, error } = useQuery("getEmails", useFetchEmails);
+  const emailsList = data?.data.filter((items) => items.type === "sent");
 
-    return(
-        <Box>
-            sent emails
-        </Box>
-    )
+  if (status === "error") {
+    console.log(error.message);
+  }
+  if (status === "loading") {
+    <Loading />;
+  }
+  if (status === "success") {
+    return (
+      <Stack
+        padding={"20px"}
+        flexDirection="column"
+        alignItems={"start"}
+        justifyContent="start"
+        sx={{ width: "100%" }}
+      >
+        <Box sx={{ margin: "0 0 30px 0" }}>filters</Box>
+        <EmailsList emailsList={emailsList} />
+      </Stack>
+    );
+  }
 }
