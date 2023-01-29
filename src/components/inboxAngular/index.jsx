69a326e077ac76a0e1d02b83ad361841139245f9
farhad <rb0214192@gmail.com>
2023-01-29 10:27:28 +0330

    refactor:add usequery and emailslist component

--- src/components/inboxAngular/index.jsx
+++ src/components/inboxAngular/index.jsx
@@ -1,10 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import { useQuery } from "react-query";
+import { useFetchEmails } from "../../hooks/useFetchEmails";
+import { Loading } from "../loading";
+import EmailsList from "../emailsList";
 
-export default function InboxAngular(){
+export default function InboxAngular() {
+  const { data, status, error } = useQuery("getEmails", useFetchEmails);
 
-return(
-    <Box>
-        Angular
-    </Box>
-)
+  const emailsList = data?.data.filter((items) => items.label === "Angular");
+
+  if (status === "error") {
+    console.log(error.message);
+  }
+  if (status === "loading") {
+    return <Loading />;
+  }
+  if (status === "success") {
+    return (
+      <Stack
+        padding={"20px"}
+        flexDirection="column"
+        alignItems={"start"}
+        justifyContent="start"
+        sx={{ width: "100%" }}
+      >
+        <Box sx={{ margin: "0 0 30px 0" }}>filters</Box>
+        <EmailsList emailsList={emailsList} />
+      </Stack>
+    );
+  }
 }
