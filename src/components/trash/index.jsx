426b64c89c91c256db10a051e8f4b9cf41814fc0
farhad <rb0214192@gmail.com>
2023-01-29 10:39:21 +0330

    refactor:add usequery and emailslist component

--- src/components/trash/index.jsx
+++ src/components/trash/index.jsx
@@ -1,10 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import { useQuery } from "react-query";
+import { useFetchEmails } from "../../hooks/useFetchEmails";
+import EmailsList from "../emailsList";
+import { Loading } from "../loading";
 
-export default function Trash(){
+export default function Trash() {
+  const { data, error, status } = useQuery("getEmails", useFetchEmails);
 
-    return(
-        <Box>
-            trash emails
-        </Box>
-    )
+  const emailsList = data?.data.filter((items) => items.type === "trash");
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+  if (status === "error") {
+    return console.log(error.message);
+  }
+  if (status === "success") {
+    return (
+      <Stack
+        padding={"20px"}
+        flexDirection="column"
+        alignItems={"start"}
+        justifyContent="start"
+        sx={{ width: "100%" }}
+      >
+        <Box sx={{ margin: "0 0 30px 0" }}> filters </Box>
+        <EmailsList emailsList={emailsList} />
+      </Stack>
+    );
+  }
 }
