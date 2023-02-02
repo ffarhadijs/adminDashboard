380a526f863df4b39f4b2d429830243a97ef968a
farhad <rb0214192@gmail.com>
2023-02-02 14:33:04 +0330

    add email page for show email details

---
+++ src/pages/EmailDetails.jsx
@@ -0,0 +1,25 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import EmailContent from "../components/emailContent";
+import { Loading } from "../components/loading";
+import { useFetchEmail } from "../hooks/useFetchEmail";
+
+export const EmailDetails = () => {
+  const { id } = useParams();
+
+  const { data, error, status } = useQuery(["getEmail", id], useFetchEmail);
+
+  if (status === "loading") {
+    <Loading />;
+  }
+  if (status === "error") {
+    console.log(error.message);
+  }
+  if (status === "success") {
+    return (
+      <>
+        <EmailContent data={data.data} />
+      </>
+    );
+  }
+};
