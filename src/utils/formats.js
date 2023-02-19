7f576f2e97b41dba650eaeed21e7787bb710112d
farhad <rb0214192@gmail.com>
2023-02-19 09:17:22 +0330

    create quill editor format file

---
+++ src/utils/formats.js
@@ -0,0 +1,6 @@
+export const formats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'video'
+  ]
