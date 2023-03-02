cbaad08e844a9ce0dc809337bd902db1fe2d69ac
farhad <rb0214192@gmail.com>
2023-03-02 19:29:39 +0330

    create hook for update todos

---
+++ src/hooks/useUpdateTodo.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "react-query";
+
+const updateTodo = (todo) => {
+  axios.put(`http://localhost:3001/todos/${todo.id}`, todo);
+};
+
+export const useUpdateTodo = () => {
+  const queryClient = useQueryClient();
+  return useMutation(updateTodo, {
+    onSuccess: () => {queryClient.invalidateQueries("getTodos")},
+  });
+};
