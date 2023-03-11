ccfeeedb21b791e5824e304cf1fb09a261895a9c
farhad <rb0214192@gmail.com>
2023-03-11 11:44:26 +0330

    create page for todos

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -11,6 +11,8 @@ import {
   Paper,
   CircularProgress,
   Checkbox,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import { useState } from "react";
 import { tokens } from "../theme";
@@ -19,40 +21,85 @@ import { useSendTodo } from "../hooks/useSendTodo";
 import { useFetchTodos } from "../hooks/useFetchTodos";
 import { useUpdateTodo } from "../hooks/useUpdateTodo";
 import { useDeleteTodo } from "../hooks/useDeleteTodo";
+import { useDeleteTodos } from "../hooks/useDeleteTodos";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useFetchTodo } from "../hooks/useFetchTodo";
+
 
 const Todos = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const { mutateAsync, isLoading } = useSendTodo();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [itemId, setItemId] = useState("");
+  const [editable, setEditable] = useState(false);
   const { data, status } = useFetchTodos();
+  const [todos, setTodos] = useState(data);
+  const { mutateAsync, isLoading } = useSendTodo();
   const { mutateAsync: mutateUpdateTodo } = useUpdateTodo();
-  const {mutateAsync: mutateDeleteTodo}=useDeleteTodo()
+  const { mutateAsync: mutateDeleteTodos } = useDeleteTodos();
+  const { mutateAsync: mutateDeleteTodo } = useDeleteTodo();
+  const { data: getEditTodo } = useFetchTodo(itemId);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (e, id) => {
+    setAnchorEl(e.currentTarget);
+    setItemId(id);
+  };
+
+
   const [todo, setTodo] = useState({
     id: Math.floor(Math.random() * 10000),
     title: "",
     isCompleted: false,
   });
-  const completedTodos=data?.data?.filter(todo=>todo.isCompleted===true)
+
+  const completedTodos = data?.filter((todo) => todo.isCompleted === true);
   const changeHandler = (e) => {
     setTodo({ ...todo, title: e.target.value });
   };
 
   const submitHandler = () => {
-    mutateAsync(todo);
+    editable && mutateUpdateTodo(todo);
+    !editable && mutateAsync(todo);
     setTodo({
       id: Math.floor(Math.random() * 10000),
       title: "",
       isCompleted: false,
     });
+    setEditable(false);
   };
   const checkedHandler = (item) => {
     mutateUpdateTodo({ ...item, isCompleted: !item.isCompleted });
   };
 
-  const clearCompletedHandler=()=>{
-    const completedTodosId=completedTodos.map(todo=>todo.id)
-    mutateDeleteTodo(completedTodosId)
-  }
+  const clearCompletedHandler = () => {
+    const completedTodosId = completedTodos.map((todo) => todo.id);
+    mutateDeleteTodos(completedTodosId);
+  };
+
+  const removeHandler = () => {
+    mutateDeleteTodo(itemId);
+    setAnchorEl(null);
+  };
+
+  const editHandler = () => {
+    setEditable(true);
+    setTodo(getEditTodo.data);
+    setAnchorEl(null);
+  };
+
+  const showAllTodos = () => {
+    setTodos(data);
+  };
+
+  const showActiveHandler = () => {
+    data?.filter((item) => item.isCompleted === false);
+  };
+  const showCompletedHandler = () => {
+    const completedTodos = data?.filter((item) => item.isCompleted === true);
+    setTodos(completedTodos);
+  };
   const Input = styled(TextField)({
     "& .MuiInputBase-input": {
       color: colors.grey[100],
@@ -70,17 +117,25 @@ const Todos = () => {
       },
     },
   });
-
+  // console.log(todos);
   return (
     <Box sx={{ padding: "40px" }}>
       <Stack flexDirection={"row"} columnGap={2} sx={{ marginBottom: "20px" }}>
-        <Button variant="contained" color="secondary">
+        <Button onClick={showAllTodos} variant="contained" color="secondary">
           All
         </Button>
-        <Button variant="contained" color="secondary">
+        <Button
+          onClick={showActiveHandler}
+          variant="contained"
+          color="secondary"
+        >
           Active
         </Button>
-        <Button variant="contained" color="secondary">
+        <Button
+          onClick={showCompletedHandler}
+          variant="contained"
+          color="secondary"
+        >
           Completed
         </Button>
       </Stack>
@@ -101,29 +156,50 @@ const Todos = () => {
         }}
       />
       {status === "success" &&
-        data?.data.map((item) => (
+        data?.map((item) => (
           <Paper
             key={item.id}
             sx={{
               marginY: "10px",
               padding: "10px",
               backgroundColor: colors.primary[600],
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
             }}
             elevation={3}
           >
-            <Checkbox
-              checked={item.isCompleted}
-              onChange={() => checkedHandler(item)}
-            />
-            <Typography variant="body1" sx={{ display: "inline-block" }}>
-              {item.title}
-            </Typography>
+            <Box>
+              <Checkbox
+                checked={item.isCompleted}
+                onChange={() => checkedHandler(item)}
+              />
+              <Typography variant="body1" sx={{ display: "inline-block" }}>
+                {item.title}
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton onClick={(e) => handleClick(e, item.id)}>
+                <MoreVertIcon />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+              >
+                <MenuItem onClick={editHandler}>Edit</MenuItem>
+                <MenuItem onClick={removeHandler}>Remove</MenuItem>
+              </Menu>
+            </Box>
           </Paper>
         ))}
       {status === "loading" && <CircularProgress />}
-      {!!completedTodos.length&&
-      <Button variant="contained" onClick={clearCompletedHandler}>clear completed</Button>
-      }
+
+      {!!completedTodos?.length && (
+        <Button variant="contained" onClick={clearCompletedHandler}>
+          clear completed
+        </Button>
+      )}
     </Box>
   );
 };
