783fb34586369f3f42f3afff47619cf53c26d98f
farhad <rb0214192@gmail.com>
2023-03-11 11:43:16 +0330

    get todos hook

--- src/hooks/useFetchTodos.js
+++ src/hooks/useFetchTodos.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
-const fetchTodos = () => axios.get("http://localhost:3001/todos");
+const fetchTodos = async () => {
+  const { data } = await axios.get("http://localhost:3001/todos");
+  return data;
+};
 
 export const useFetchTodos = () => {
   return useQuery("getTodos", fetchTodos);
