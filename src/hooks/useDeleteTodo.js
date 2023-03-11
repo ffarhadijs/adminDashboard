2a82eb02be755ac516848981a6f507e2482b4ec3
farhad <rb0214192@gmail.com>
2023-03-11 11:40:52 +0330

    delete todo hook

--- src/hooks/useDeleteTodo.js
+++ src/hooks/useDeleteTodo.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useMutation, useQueryClient } from "react-query";
 
 const deleteTodo = (id) => {
-        axios.delete(`http://localhost:3001/todos/${id}`);
+  axios.delete(`http://localhost:3001/todos/${id}`);
 };
 
 export const useDeleteTodo = () => {
