d855d5a5d953aa5b93658733732951c560fc9f05
farhad <rb0214192@gmail.com>
2023-03-16 12:35:25 +0330

    create page for notes

---
+++ src/pages/Notes.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import NoteInput from "../components/NoteInput/NoteInput";
+
+export default function Notes() {
+  return (
+    <Box padding={"20px"}>
+      <NoteInput />
+    </Box>
+  );
+}
