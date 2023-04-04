94d193c357488ac35f30fb850c17bb65ef5871d2
farhad <rb0214192@gmail.com>
2023-04-04 12:59:50 +0430

    add color feature and make clean

--- src/Components/NotesList/NotesList.jsx
+++ src/Components/NotesList/NotesList.jsx
@@ -1,13 +1,6 @@
-import {
-  Card,
-  CardContent,
-  CircularProgress,
-  Grid,
-  Paper,
-  Typography,
-  useTheme,
-} from "@mui/material";
+import { CircularProgress, Grid, Typography, useTheme } from "@mui/material";
 import { Box } from "@mui/system";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { useFetchNotes } from "../../hooks/useFetchNotes";
 import { tokens } from "../../theme";
@@ -16,7 +9,6 @@ export default function NotesList() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const { data: notesList, isLoading } = useFetchNotes();
-  console.log(notesList);
   if (isLoading) {
     return (
       <Box
@@ -28,23 +20,26 @@ export default function NotesList() {
   }
   return (
     <Grid container spacing={2} paddingY={2}>
-      {notesList?.slice(0).reverse().map((note) => (
-        <Grid item xs={12} sm={6} md={4} key={note.id}>
-          <Link to={`${note.id}`}>
-            <Box
-              padding={2}
-              sx={{
-                backgroundColor: colors.primary[600],
-                border: `1px solid ${colors.primary[400]}`,
-                overflow: "auto",
-                textOverflow: "ellipsis",
-              }}
-            >
-              <Typography variant="body1">{note.name}</Typography>
-            </Box>
-          </Link>
-        </Grid>
-      ))}
+      {notesList
+        ?.slice(0)
+        .reverse()
+        .map((item) => (
+          <Grid item xs={12} sm={6} md={4} key={item.id}>
+            <Link to={`${item.id}`}>
+              <Box
+                padding={2}
+                sx={{
+                  backgroundColor: item.color,
+                  border: `1px solid ${colors.primary[400]}`,
+                  overflow: "auto",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                <Typography variant="body1">{item.name}</Typography>
+              </Box>
+            </Link>
+          </Grid>
+        ))}
     </Grid>
   );
 }
