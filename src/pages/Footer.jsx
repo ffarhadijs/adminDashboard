948fd87a99c7928130e9b08922f07fcce6ec6e30
farhad <rb0214192@gmail.com>
2023-04-09 19:48:09 +0430

    create footer page with three footer elements

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Box, Grid } from "@mui/material";
+import React from "react";
+import FooterOne from "../components/FooterElements/FooterOne";
+import FooterTwo from "../components/FooterElements/FooterTwo";
+import FooterThree from "../components/FooterElements/FooterThree";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Grid container rowGap={2} p={2}>
+      <Grid item xs={12}>
+        <FooterOne />
+      </Grid>
+      <Grid item xs={12}>
+        <FooterTwo />
+      </Grid>
+      <Grid item xs={12}>
+        <FooterThree />
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Footer
+export default Footer;
